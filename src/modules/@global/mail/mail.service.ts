55e9d1ee5c96e2c725cc86154217ed1db2a3f51a
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2024-05-10 13:57:48 +0400

    final build for email service

--- src/modules/@global/mail/mail.service.ts
+++ src/modules/@global/mail/mail.service.ts
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 import { InjectEnv } from '../env/env.decorator';
 import { EnvService } from '../env/env.service';
@@ -22,139 +22,21 @@ export class MailService implements OnModuleInit {
   }
 
   async send(data: SendSimpleParams) {
-    this.logger.debug('Sending mail...');
-    this.logger.debug(JSON.stringify(data, null, 2));
+    this.logger.debug('Sending mail... ' + JSON.stringify(data, null, 2));
 
-    const info = await this.resend.emails.send({
+    const { data: info, error } = await this.resend.emails.send({
       from: this.from,
       to: data.to,
       subject: data.subject,
       text: data.text,
     });
 
-    this.logger.debug('Finished sending mail...');
-    this.logger.debug(JSON.stringify(info, null, 2));
+    if (!info || error) {
+      this.logger.debug('Error sending mail... ' + JSON.stringify({ data: info, error }, null, 2));
+      throw new BadRequestException('Problem with sending email');
+    }
 
-    // return info;
+    this.logger.debug('Finished sending mail...');
+    return info.id;
   }
 }
-// import nodemailer from 'nodemailer';
-// import SMTPTransport from 'nodemailer/lib/smtp-transport';
-// import { BadRequestException, Injectable, Logger, OnModuleInit } from '@nestjs/common';
-
-// import { InjectEnv } from '../env/env.decorator';
-// import { EnvService } from '../env/env.service';
-// import { SendSimpleParams } from './mail.type';
-
-// @Injectable()
-// export class MailService implements OnModuleInit {
-//   private readonly logger = new Logger(MailService.name);
-//   private transporter: nodemailer.Transporter<SMTPTransport.SentMessageInfo>;
-//   private isSandbox: boolean = false;
-//   private from: string;
-
-//   constructor(
-//     @InjectEnv()
-//     private readonly envService: EnvService,
-//   ) {}
-
-//   async onModuleInit() {
-//     this.isSandbox = this.envService.get('ENABLE_MAIL_SANDBOX');
-
-//     if (this.isSandbox) {
-//       return;
-//     }
-
-//     this.from = this.envService.get('MAIL_FROM');
-
-//     const transportOptions = <SMTPTransport.Options>{
-//       host: this.envService.get('MAIL_URL'),
-//       port: 465,
-//       secure: true,
-//       auth: {
-//         user: this.envService.get('MAIL_USERNAME'),
-//         pass: this.envService.get('MAIL_PASSWORD'),
-//       },
-//       // ignoreTLS: true,
-//     };
-
-//     this.transporter = nodemailer.createTransport(transportOptions);
-
-//     this.logger.verbose('Verifying mail connection...');
-
-//     console.log('='.repeat(20));
-//     console.log(this.envService.getInstance());
-//     console.log({ sandbox: this.isSandbox });
-//     console.log(transportOptions);
-//     console.log('='.repeat(20));
-
-//     const isVerified = await new Promise<{ data: boolean | null; error: Error | null }>(resolve => {
-//       this.transporter.verify((error, _success) => {
-//         if (error) {
-//           console.log('='.repeat(20));
-//           console.log(error);
-//           resolve({ data: null, error });
-//         } else {
-//           console.log('='.repeat(20));
-//           console.log('Server is ready to take our messages');
-//           resolve({ data: true, error: null });
-//         }
-//       });
-//     });
-
-//     this.logger.verbose(
-//       JSON.stringify({
-//         isVerified,
-//         name: this.transporter.transporter.name,
-//         host: this.envService.get('MAIL_URL'),
-//       }),
-//     );
-//   }
-
-//   async send(data: SendSimpleParams): Promise<SMTPTransport.SentMessageInfo> {
-//     this.logger.debug('Sending mail...');
-//     this.logger.debug(JSON.stringify(data, null, 2));
-
-//     const { data: info, error } = await new Promise<{
-//       data: SMTPTransport.SentMessageInfo | null;
-//       error: Error | null;
-//     }>(resolve => {
-//       // send mail
-//       this.transporter.sendMail(
-//         {
-//           from: this.from,
-//           to: data.to,
-//           subject: data.subject,
-//           text: data.text,
-//         },
-//         (error, info) => {
-//           if (error) {
-//             console.log('='.repeat(20));
-//             console.log(error);
-//             resolve({ data: null, error });
-//           } else {
-//             console.log('='.repeat(20));
-//             console.log('Mail sent');
-//             resolve({ data: info, error: null });
-//           }
-//         },
-//       );
-//     });
-
-//     if (error || !info) {
-//       throw new BadRequestException('Error sending email');
-//     }
-
-//     // const info = await this.transporter.sendMail({
-//     //   from: this.from,
-//     //   to: data.to,
-//     //   subject: data.subject,
-//     //   text: data.text,
-//     // });
-
-//     this.logger.debug('Finished sending mail...');
-//     this.logger.debug(JSON.stringify(info, null, 2));
-
-//     return info;
-//   }
-// }
